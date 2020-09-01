0dfb984285157996eb7f420450623ab6895df335
Todd Ransom <toddransom@me.com>
2020-09-01 18:22:45 -0400

    ugly, but working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
+
+  const [temp, setTemp] = useState("Loading...")
+//var temp = '0.00F'
+//var temp = '';
+fetch('http://localhost:3000/topic/bus-1-temp/current')
+  .then( res => res.json() )
+  .then( (result) => {
+    setTemp(result.value);
+  },
+  (error) => {
+    console.log(error);
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Card border='dark' bg='success' text='white'><Card.Body>
+              <Card.Header>Bus Temp</Card.Header>
+              <Card.Text>{temp}</Card.Text>
+            </Card.Body></Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
