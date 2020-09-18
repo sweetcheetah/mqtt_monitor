34dda7c45c696e8cd0bf3860e8763f85fbfbcb39
Todd Ransom <toddransom@me.com>
2020-09-18 14:46:57 -0400

    bff_url var, will soon move to environment

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ import Card from 'react-bootstrap/Card'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const bff_url = 'http://10.0.0.9:3000';
+//const bff_url = proc.env.BFF_URL;
+
 
 function App() {
-
   const [temp, setTemp] = useState("Loading...");
   const [humidity, setHumidity] = useState("Loading...");
+  const [tempUpdated, setTempUpdated] = useState("never");
+  const [humidityUpdated, setHumidityUpdated] = useState("never");
 
-  fetch('http://10.0.0.200:3000/topic/bus-1-temp/current')
+  fetch(`${bff_url}/topic/bus-1-temp/current`)
     .then( res => res.json() )
     .then( (result) => {
       setTemp(result.value);
+      setTempUpdated(result.lastUpdated);
     },
     (error) => {
       console.log(error);
     });
  
-  fetch('http://10.0.0.200:3000/topic/bus-1-humidity/current')
+  fetch(`${bff_url}/topic/bus-1-humidity/current`)
     .then( res => res.json() )
     .then( (result) => {
       setHumidity(result.value);
+      setHumidityUpdated(result.lastUpdated);
     },
     (error) => {
       console.log(error);
@@ -38,10 +44,12 @@ function App() {
             <Card border='dark' bg='success' text='white'><Card.Body>
               <Card.Header>Bus Temp</Card.Header>
               <Card.Text>{temp}</Card.Text>
+              <Card.Text>Last Updated: {tempUpdated}</Card.Text>
             </Card.Body></Card>
             <Card border='dark' bg='success' text='white'><Card.Body>
               <Card.Header>Bus Humidity</Card.Header>
               <Card.Text>{humidity}</Card.Text>
+              <Card.Text>Last Updated: {humidityUpdated}</Card.Text>
             </Card.Body></Card>
           </Col>
         </Row>
